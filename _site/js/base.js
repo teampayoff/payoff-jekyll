7a9f22c23840ecdfc97c160eb628834ea7fb1aeb
John Choura Jr <jchoura@payoff.com>
2015-10-06 11:18:36 -0700

    designing for homepage, lots of custom lines of CSS

---
+++ _site/js/base.js
@@ -0,0 +1,13 @@
+$(function() {
+    //caches a jQuery object containing the header element
+    var header = $("nav");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 300) {
+            header.removeClass('navbar-naked').addClass("navbar-default");
+        } else {
+            header.removeClass("navbar-default").addClass('navbar-naked');
+        }
+    });
+});
